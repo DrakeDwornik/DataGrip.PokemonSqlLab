a3090fdf43e9ad36a70d766098c06532b496e860
Drake Dwornik <drake@drakedwornik.com>
2021-11-08 21:23:23 -0500

    fixed column heading 'Stat Total'

--- answers/Part 4.sql
+++ answers/Part 4.sql
@@ -1,8 +1,8 @@
 # Directions: Write a query that returns the following collumns:
-# The highest total stats (excluding hp related ones) at highest level = strongest
+# The highest combined stats (excluding hp) at level 100 = strongest
 
 select poke.name 'Pokemon Name', t.trainername 'Trainer Name', pt.pokelevel Level,
-       prime_type.name 'Primary Type', second_type.name "Secondary Type", pt.attack+pt.defense+pt.defense+pt.spatk+pt.spdef+pt.speed as Stat_Total
+       prime_type.name 'Primary Type', second_type.name "Secondary Type", pt.attack+pt.defense+pt.defense+pt.spatk+pt.spdef+pt.speed as "Stat Total"
 from pokemon_trainer pt JOIN pokemons poke on pt.pokemon_id = poke.id JOIN types second_type on poke.secondary_type = second_type.id
                         JOIN types prime_type on poke.primary_type = prime_type.id JOIN trainers t on pt.trainerID = t.trainerID
 ORDER BY pt.pokelevel DESC, pt.attack+pt.defense+pt.defense+pt.spatk+pt.spdef+pt.speed DESC;
